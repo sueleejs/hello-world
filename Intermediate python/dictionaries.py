62544824b4aef2616352c05762be3243a661651d
SUE LEE <48482642+sueleejs@users.noreply.github.com>
2022-05-29 16:39:31 +0200

    Update and rename dictionaries to dictionaries.py

---
+++ Intermediate python/dictionaries.py
@@ -0,0 +1,33 @@
+#Countries contains the names of some European countries. capitals lists the corresponding names of their capital
+# Definition of countries and capital
+countries = ['spain', 'france', 'germany', 'norway']
+capitals = ['madrid', 'paris', 'berlin', 'oslo']
+
+# Get index of 'germany': ind_ger
+ind_ger = countries.index('germany')
+
+# Use ind_ger to print out capital of Germany
+print(capitals[ind_ger])
+
+# Definition of countries and capital
+countries = ['spain', 'france', 'germany', 'norway']
+capitals = ['madrid', 'paris', 'berlin', 'oslo']
+
+# From string in countries and capitals, create dictionary europe
+europe = {'spain':'madrid', 
+          'france': 'paris', 
+          'germany':'berlin', 
+          'norway':'oslo' 
+         }
+
+# Print europe
+print(europe)
+
+# Definition of dictionary
+europe = {'spain':'madrid', 'france':'paris', 'germany':'berlin', 'norway':'oslo' }
+
+# Print out the keys in europe
+print(europe.keys())
+
+# Print out value that belongs to key 'norway'
+print(europe['norway'])
