755d451c090b00fdd9d35ccc3ed318f540b5ae42
SUE LEE <48482642+sueleejs@users.noreply.github.com>
2022-05-29 22:58:54 +0200

    Update and rename Loopover.py to Loop.py

--- Intermediate python/Loop.py
+++ Intermediate python/Loop.py
@@ -1,3 +1,38 @@
+# areas list
+areas = [11.25, 18.0, 20.0, 10.75, 9.50]
+
+# Code the for loop
+for area in areas :
+    print(area)
+
+# areas list
+areas = [11.25, 18.0, 20.0, 10.75, 9.50]
+
+# Change for loop to use enumerate() and update print()
+for index, area in enumerate(areas) :
+    print("room " + str(index) + ": " + str(area))
+
+# areas list
+areas = [11.25, 18.0, 20.0, 10.75, 9.50]
+
+# Code the for loop
+for index, area in enumerate(areas) :
+    print("room " + str(index +1) + ": " + str(area))
+
+# house list of lists
+house = [["hallway", 11.25], 
+         ["kitchen", 18.0], 
+         ["living room", 20.0], 
+         ["bedroom", 10.75], 
+         ["bathroom", 9.50]]
+         
+# Build a for loop from scratch
+for x in house:
+    print("the " + str(x[0]) + " is " + str(x[1]) + " sqm")
+
+
+#loop data structures 
+
 # Definition of dictionary
 europe = {'spain':'madrid', 'france':'paris', 'germany':'berlin',
           'norway':'oslo', 'italy':'rome', 'poland':'warsaw', 'austria':'vienna' }
