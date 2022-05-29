07063b1978df694b603343e5ac3aa9bb2e0a3716
SUE LEE <48482642+sueleejs@users.noreply.github.com>
2022-05-29 17:51:58 +0200

    Update dictionariespandas.py

--- Intermediate python/dictionariespandas.py
+++ Intermediate python/dictionariespandas.py
@@ -139,3 +139,14 @@ print(cars[0:3])
 
 # Print out fourth, fifth and sixth observation
 print(cars[3:6])
+
+# Import cars data
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Print out drives_right value of Morocco
+print(cars.loc["MOR", "drives_right"])
+
+
+# Print sub-DataFrame
+print(cars.iloc[[4,5], [1,2]])
