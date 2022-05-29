0a2105fca73d988f38ffb46cfe824ebee837e7ff
SUE LEE <48482642+sueleejs@users.noreply.github.com>
2022-05-29 19:00:12 +0200

    Update Comparison Operation.py

--- Intermediate python/Comparison Operation.py
+++ Intermediate python/Comparison Operation.py
@@ -57,3 +57,19 @@ car_maniac = cars[many_cars]
 
 # Print car_maniac
 print(car_maniac)
+
+# Import cars data
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Import numpy, you'll need this
+import numpy as np
+
+# Create medium: observations with cars_per_cap between 100 and 500
+cpc = cars['cars_per_cap']
+between = np.logical_and(cpc> 100, cpc <500)
+medium = cars[between]
+
+print(medium)
+
+# Print medium
