ee2eb017e86d53a4e099618441acd64d5caad28c
SUE LEE <48482642+sueleejs@users.noreply.github.com>
2022-05-29 22:10:54 +0200

    Update Loop over.py

--- Intermediate python/Loop over.py
+++ Intermediate python/Loop over.py
@@ -5,3 +5,13 @@ europe = {'spain':'madrid', 'france':'paris', 'germany':'berlin',
 # Iterate over europe
 for k, v in europe.items():
     print("the capital of " + str(k) + " is " +str(v))
+
+# Import numpy as np
+import numpy as np
+
+# For loop over np_height
+for x in np_height:
+    print(str(x) + " inches")
+# For loop over np_baseball
+for x in np.nditer(np_baseball) :
+    print(x)
