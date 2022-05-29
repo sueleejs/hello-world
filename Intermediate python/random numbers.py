f3a5b459e93e8b80bc2b8fed39eb9c210dda757b
SUE LEE <48482642+sueleejs@users.noreply.github.com>
2022-05-29 23:04:54 +0200

    Update random numbers.py

--- Intermediate python/random numbers.py
+++ Intermediate python/random numbers.py
@@ -1,1 +1,18 @@
+import numpy as np
+np.random.rand()
+np.random.seed(123)
 
+#coin toss
+np.random.seed(123)
+coin = np.random.radint(0,2)
+print(coin)
+
+import numpy as np
+np.random.seed(123)
+coin = np.random.randint(0,2) #randomly generate 0 or 1
+print(coin)
+if coin == 0:
+  print("heads")
+else:
+  print("tails")
+  
