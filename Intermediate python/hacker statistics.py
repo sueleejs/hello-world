2cc78ab274f63add75fb99ab8ceba94fb2dc7d76
SUE LEE <48482642+sueleejs@users.noreply.github.com>
2022-05-30 10:07:18 +0200

    Update hacker statistics.py

--- Intermediate python/hacker statistics.py
+++ Intermediate python/hacker statistics.py
@@ -1,3 +1,11 @@
+#Instruction
+Roll the dice. Use randint() to create the variable dice.
+Finish the if-elif-else construct by replacing ___:
+If dice is 1 or 2, you go one step down.
+if dice is 3, 4 or 5, you go one step up.
+Else, you throw the dice again. The number of eyes is the number of steps you go up.
+Print out dice and step. Given the value of dice, was step updated correctly?
+
 # Import numpy as np
 import numpy as np
 
