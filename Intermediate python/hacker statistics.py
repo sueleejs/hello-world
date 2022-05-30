32aeb60fa342518a4fc8abdbb4f02ab82ee78c10
SUE LEE <48482642+sueleejs@users.noreply.github.com>
2022-05-30 10:56:43 +0200

    Update hacker statistics.py

--- Intermediate python/hacker statistics.py
+++ Intermediate python/hacker statistics.py
@@ -24,3 +24,68 @@ print(np.random.randint(1,7))
 
 # Use randint() again
 print(np.random.randint(1,7))
+
+# NumPy is imported, seed is set
+
+# Starting step
+step = 50
+
+# Roll the dice
+dice = np.random.randint(1,7)
+
+# Finish the control construct
+if dice <= 2 :
+    step = step - 1
+elif dice < 6:
+    step = step + 1
+else :
+    step = step + np.random.randint(1,7)
+
+# Print out dice and step
+print(dice)
+print(step)
+
+#Haeds or Tails
+import numpy as np
+np.random.seed(123)
+outcomes = []
+for x in range(10) :
+  coin = np.random.randint(0, 2)
+  if coin == 0 :
+      outcomes.append("heads")
+  else :
+      outcomes.append("tails")
+print(outcomes)
+
+#Haeds or Tails: Random Walk
+import numpy as np
+np.random.seed(123)
+tails = [0]
+for x in range(10):
+  coin = np.random.randint(0, 2)
+  tails.append(tails[x] + coin)
+print(tails)
+  
+  
+# NumPy is imported, seed is set
+
+# Initialize random_walk
+random_walk = [0]
+
+for x in range(100) :
+    step = random_walk[-1]
+    dice = np.random.randint(1,7)
+
+    if dice <= 2:
+        # Replace below: use max to make sure step can't go below 0
+        step = step - 1
+    elif dice <= 5:
+        step = step + 1
+    else:
+        step = step + np.random.randint(1,7)
+
+    random_walk.append(step)
+
+print(random_walk)
+
+
