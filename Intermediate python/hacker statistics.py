e322bc24db38f60c2fba322e5eeaf223c86ffde4
SUE LEE <48482642+sueleejs@users.noreply.github.com>
2022-05-30 10:04:26 +0200

    Update hacker statistics.py

--- Intermediate python/hacker statistics.py
+++ Intermediate python/hacker statistics.py
@@ -6,3 +6,13 @@ np.random.seed(123)
 
 # Generate and print random float
 print(np.random.rand())
+
+# Import numpy and set seed
+import numpy as np
+np.random.seed(123)
+
+# Use randint() to simulate a dice
+print(np.random.randint(1,7))
+
+# Use randint() again
+print(np.random.randint(1,7))
